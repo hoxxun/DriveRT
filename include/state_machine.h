2a5573713dc652b95d9b90d85ca6044299f7e945
HJ J <junghojun1023@gmail.com>
2026-09-10 14:23:07 +0900

    refactor: split state machine into source and header files

---
+++ include/state_machine.h
@@ -0,0 +1,20 @@
+typedef enum
+{
+    STATE_STOP, 
+    STATE_RUNNING, 
+    STATE_ERROR, 
+    STATE_RECOVERY
+} State;
+
+typedef enum
+{
+    EVENT_RUN, 
+    EVENT_STOP, 
+    EVENT_FAULT, 
+    EVENT_RECOVER, 
+    EVENT_RECOVERY_OK, 
+    EVENT_RECOVERY_FAIL
+} Event;
+
+
+State getNextState(State current_state, Event current_event);
--- src/main.c
+++ src/main.c
@@ -1,22 +1,5 @@
 #include <stdio.h>
-
-typedef enum
-{
-    STATE_STOP, 
-    STATE_RUNNING, 
-    STATE_ERROR, 
-    STATE_RECOVERY
-} State;
-
-typedef enum
-{
-    EVENT_RUN, 
-    EVENT_STOP, 
-    EVENT_FAULT, 
-    EVENT_RECOVER, 
-    EVENT_RECOVERY_OK, 
-    EVENT_RECOVERY_FAIL
-} Event;
+#include "../include/state_machine.h"
 
 //문자열 배열은 pointer를 쓰는구나
 //값 수정 안시킬거니까 const 붙임
@@ -30,49 +13,13 @@ const char * state_buf[] = {
 int main(void)
 {
     State current_state = STATE_STOP;
-    Event current_event = EVENT_RUN;
+    Event current_event = EVENT_FAULT;
 
     printf("[LOG] current state: %s\n", state_buf[current_state]);
 
-    switch(current_state)
-    {
-    case STATE_STOP:
-        if(current_event == EVENT_RUN)
-        {
-            current_state = STATE_RUNNING;
-        }
-        if(current_event == EVENT_FAULT)
-        {
-            current_state = STATE_ERROR;
-        }
-        break;
-    case STATE_RUNNING:
-        if(current_event == EVENT_STOP)
-        {
-            current_state = STATE_STOP;
-        }
-        if (current_event == EVENT_FAULT)
-        {
-            current_state = STATE_ERROR;
-        }
-        break;
-    case STATE_ERROR:
-        if(current_event == EVENT_RECOVER)
-        {
-            current_state = STATE_RECOVERY;
-        }
-        break;
-    case STATE_RECOVERY:
-        if(current_event == EVENT_RECOVERY_OK)
-        {
-            current_state = STATE_STOP;
-        }
-        if(current_event == EVENT_RECOVERY_FAIL)
-        {
-            current_state = STATE_ERROR;
-        }
-        break;
-    }
-
+    current_state = getNextState(current_state, current_event);
+    
     printf("[LOG] current state: %s\n", state_buf[current_state]);
+
+    return 0;
 }
