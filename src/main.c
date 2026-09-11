6dc484a19a4074f990aa4528c24e9839b0c907b1
HJ J <junghojun1023@gmail.com>
2026-09-12 00:09:46 +0900

    feat: add state-specific actions

--- src/main.c
+++ src/main.c
@@ -10,16 +10,63 @@ const char * state_buf[] = {
     "STATE_RECOVERY"
 };
 
+
+void stateHandler(State current_state)
+{
+    switch (current_state)
+    {
+    case STATE_STOP:
+        printf("[LOG] current state: %s\n", state_buf[current_state]);  
+        printf("PWM을 0으로 설정\n");
+        printf("모터 정지\n");
+        break;
+    
+    case STATE_RUNNING:
+        printf("[LOG] current state: %s\n", state_buf[current_state]);  
+        printf("PWM ON\n"); 
+        printf("모터 구동\n");
+        break;
+
+    case STATE_ERROR:
+        printf("[LOG] current state: %s\n", state_buf[current_state]);  
+        printf("PWM을 0으로 설정\n");
+        printf("오류 원인 확인중...\n");
+        printf("오류 원인 확인\n");
+        break;
+    
+    case STATE_RECOVERY:
+        printf("[LOG] current state: %s\n", state_buf[current_state]);  
+        printf("오류 복구 시작\n");
+        break;
+    }
+}
+
+
 int main(void)
 {
+
+    /*
+    EVENT_RUN, 
+    EVENT_STOP, 
+    EVENT_FAULT, 
+    EVENT_RECOVER, 
+    EVENT_RECOVERY_OK, 
+    EVENT_RECOVERY_FAIL
+    */
+
     State current_state = STATE_STOP;
-    Event current_event = EVENT_FAULT;
 
-    printf("[LOG] current state: %s\n", state_buf[current_state]);
+    current_state = getNextState(current_state, EVENT_RUN);
+    stateHandler(current_state);
 
-    current_state = getNextState(current_state, current_event);
-    
-    printf("[LOG] current state: %s\n", state_buf[current_state]);
+    current_state = getNextState(current_state, EVENT_FAULT);
+    stateHandler(current_state);
+
+    current_state = getNextState(current_state, EVENT_RECOVER);
+    stateHandler(current_state);
+
+    current_state = getNextState(current_state, EVENT_RECOVERY_OK);
+    stateHandler(current_state);
 
     return 0;
 }
