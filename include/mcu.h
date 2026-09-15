3cacbce501712f3db594004237eeb20c1b45bea6
HJ J <junghojun1023@gmail.com>
2026-09-15 20:09:59 +0900

    feat: add heartbeat handling and last receive time tracking

--- include/mcu.h
+++ include/mcu.h
@@ -1,8 +1,15 @@
 #include "command.h"
 #include "state_machine.h"
 #include "actuator.h"
+#include <time.h>
 
 extern Actuator actuator;
 
 int parseCommandMessage(char * buf, Command * cmd);
 void updateActuator(State current_state, Command cmd);
+
+typedef struct 
+{
+    time_t last_heartbeat_time;
+} Heartbeat;
+
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h> //atoi 
+#include <time.h>
 #include "../include/state_machine.h"
 #include "../include/actuator.h"
 #include "../include/ap.h"
@@ -21,6 +22,9 @@ void send_receive_CommandMessage(char * tx_buf, char * rx_buf)
 
 int processCommandMessage(char * rx_buf, Command * mcu_cmd, State * current_state)
 {
+    int heartbeat_timeout = 5;
+
+
     //MCU에서 CommandMessgae 파싱 후 mcu_cmd 구조체 초기화
     int result = parseCommandMessage(rx_buf, mcu_cmd);
     if(result == FAIL)
@@ -43,6 +47,28 @@ int processCommandMessage(char * rx_buf, Command * mcu_cmd, State * current_stat
     return SUCCESS;
 }
 
+int send_receive_heartbeat(char * heartbeat_tx_buf, char * heartbeat_rx_buf, Heartbeat * heartbeat)
+{
+    printf("[AP] Hearbeat 송신: %s\n", heartbeat_tx_buf);
+    
+    printf("\n");
+
+    strcpy(heartbeat_rx_buf, heartbeat_tx_buf);
+    if(strcmp(heartbeat_rx_buf, "HeartBeat") != 0)
+    {
+        return FAIL;
+    }
+    time_t t = time(NULL);
+    heartbeat->last_heartbeat_time = t;
+    printf("[MCU] Heartbeat 수신: %s\n", heartbeat_rx_buf);
+    printf("%lu\n", heartbeat->last_heartbeat_time);
+    printf("\n");
+
+
+    return SUCCESS;
+}
+
+
 int main(void)
 {
     State current_state = STATE_STOP; //MCU
@@ -53,12 +79,19 @@ int main(void)
     char tx_buf[50];
     char rx_buf[50];
 
-    while(1)
-    {
-        //AP에서 명령어 입력 후 ap_cmd 구조체 초기화
-        int result1 = commandInput(&ap_cmd);
+    Heartbeat heartbeat;
+    char heartbeat_tx_buf[50] = {"HeartBeat"};
+    char heartbeat_rx_buf[50];
 
-        if(result1 == -1) continue;
+    while(1)
+    {   
+        //HEARTBEAT
+        int result1 = send_receive_heartbeat(heartbeat_tx_buf, heartbeat_rx_buf, &heartbeat);
+        if(result1 == FAIL) continue;
+
+        //AP에서 명령어 입력 후 ap_cmd 구조체 초기화
+        int result2 = commandInput(&ap_cmd);
+        if(result2 == FAIL) continue;
 
         //AP에서 ap_cmd 구조체를 보고 CommandMessgae 생성
         buildCommandMessage(ap_cmd, tx_buf);
